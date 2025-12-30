00e26176af9b83595b51864ca8ff6fa64d86b7d9
Magde-98 <magdehes15@gmail.com>
2025-12-30 13:20:06 +0200

    Fix product page params typing

--- app/(component)/product/[id]/page.tsx
+++ app/(component)/product/[id]/page.tsx
@@ -5,9 +5,9 @@ import AddBtn from '@/app/_component/addBtn/page';
 export default async function ProductDetails({
     params,
 }: {
-    params: { id: string };
+    params: Promise<{ id: string }>;
 }) {
-    const { id } = params;
+    const { id } = await params; 
 
     const { data } = await GetProductDetails(id);
 
